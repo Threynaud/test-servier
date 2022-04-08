3925fde53a37514f9a3903b4704f214eae73a311
Thomas Reynaud <thmsreynaud@gmail.com>
2022-04-08 16:24:36 +0200

    feat(marts): add output steps and output json file

---
+++ graph_builder/marts_steps/fct_drugs_graph.py
@@ -0,0 +1,29 @@
+import os
+
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+INT_DRUGS_CLINICAL_TRIALS_DEPS_PATH = os.environ.get("INT_DRUGS_CLINICAL_TRIALS_DEPS_PATH")
+INT_DRUGS_PUBMED_DEPS_PATH = os.environ.get("INT_DRUGS_PUBMED_DEPS_PATH")
+FCT_DRUGS_DEPS_PATH = os.environ.get("FCT_DRUGS_DEPS_PATH")
+
+
+def fct_drugs_graph_building(int_drugs_clinical_trials_deps_path, int_drugs_pubmed_deps_path):
+    """ """
+    int_drugs_clinical_trials_deps_df = pd.read_csv(int_drugs_clinical_trials_deps_path)
+    int_drugs_pubmed_deps_df = pd.read_csv(int_drugs_pubmed_deps_path)
+    fct_drugs_graph_df = pd.concat([int_drugs_clinical_trials_deps_df, int_drugs_pubmed_deps_df])
+
+    return fct_drugs_graph_df
+
+
+def main():
+    """ """
+    fct_drugs_graph_df = fct_drugs_graph_building(INT_DRUGS_CLINICAL_TRIALS_DEPS_PATH, INT_DRUGS_PUBMED_DEPS_PATH)
+    fct_drugs_graph_df.to_csv(FCT_DRUGS_DEPS_PATH, index=True, index_label="dependency_id")
+
+
+if __name__ == "__main__":
+    main()
---
+++ graph_builder/output_step/drugs_graph_to_json.py
@@ -0,0 +1,25 @@
+import os
+
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+FCT_DRUGS_DEPS_PATH = os.environ.get("FCT_DRUGS_DEPS_PATH")
+OUTPUT_DRUGS_GRAPH_JSON_PATH = os.environ.get("OUTPUT_DRUGS_GRAPH_JSON_PATH")
+
+
+def output_processing(fct_drugs_deps_path):
+    """ """
+    fct_drugs_deps_df = pd.read_csv(fct_drugs_deps_path)
+    return fct_drugs_deps_df
+
+
+def main():
+    """ """
+    fct_drugs_deps_df = output_processing(FCT_DRUGS_DEPS_PATH)
+    fct_drugs_deps_df.to_json(OUTPUT_DRUGS_GRAPH_JSON_PATH, orient="records", indent=2, force_ascii=False)
+
+
+if __name__ == "__main__":
+    main()
